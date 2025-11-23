e48051f0361f08a43497a0c5baeeeccbcc397907
itinstructor <loringw@wncc.edu>
2025-11-23 11:12:40 -0700

    Add Sarah and the Isopods Video

--- create_photo_table.py
+++
@@ -1,7 +0,0 @@
-from blog.models import Photo
-from database import db
-from main_app import app
-
-with app.app_context():
-    db.create_all()
-    print("Photo table created (if it did not already exist).")
